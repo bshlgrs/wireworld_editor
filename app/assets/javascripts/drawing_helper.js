fab90f99b144f457558df5ede3f65a0317f89be3
emilycw <emilycw@users.noreply.github.com>
2014-12-02 14:35:01 +1100

    Update drawing_helper.js

--- app/assets/javascripts/drawing_helper.js
+++ app/assets/javascripts/drawing_helper.js
@@ -1,22 +1,30 @@
 $(function () {
   var Helper = window.Helper = {};
 
-  Helper.drawLines = function(x0, y0, x1, y1, callback) {
+  Helper.drawLines function(x0, y0, x1, y1, callback) {
     var dx = x1 - x0;
     var dy = y1 - y0;
+    var xo = 1;
+    var yo = 1;
+    if (x1 == x0){
+        xo = 0;
+    }
+    if (y1 == y0){
+        yo=0
+    }
     if (y0 > y1){
         var py = -1;
         var yf = y1-1;
     }else{
         var py = 1;
-        var yf = y1+1;        
+        var yf = y1+1;       
     }
     if (x0 > x1){
         var px = -1;
         var xf = x1-1;
     }else{
         var px = 1;
-        var xf = x1+1; 
+        var xf = x1+1;
     }
     if (Math.abs(dx) >= Math.abs(dy)){
         var D = (2*dy - dx)*py;
@@ -24,22 +32,26 @@ $(function () {
         var y = y0;
         var x = x0;
         for (x = (x0+px); x != xf; x = x+px){
-            if (D > 0){
+            if ((D*yo) > 0){
+                //writeToLog("D > 0");
                 y = y+py;
                 callback(x,y);
                 D = D + py*(2*dy-2*dx);
             } else {
+                //writeToLog("D =< 0");
                 callback(x,y);
                 D = D + py*(2*dy);
             }
         }
     } else {
+        writeToLog("dy > dx");
         var D = (2*dx - dy)*px;
         callback(x0,y0);
         var y = y0;
         var x = x0;
         for (y = (y0+py); y != yf; y = y+py){
-            if (D > 0){
+            if ((D*xo) > 0){
+                writeToLog("change x");
                 x = x+px;
                 callback(x,y);
                 D = D + px*(2*dx-2*dy);
@@ -48,7 +60,7 @@ $(function () {
                 D = D + px*(2*dx);
             }
         }
-    }     
+    }    
 };
 
 })
